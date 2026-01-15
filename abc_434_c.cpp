bb5ccc00865079eff3d282707370ab089f9b7fae
janak <janak@forgejo.com>
2026-01-15 16:22:24 +0530

    Update recent to lon long

---
+++ abc_434_c.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, h;
+    cin>>n>>h;
+
+    vector<vector<int>> v(n, vector<int> (3, 0));
+
+    for(int i = 0 ; i < n ; i++){
+        cin>>v[i][0]>>v[i][1]>>v[i][2];
+    }
+
+    int lb = h, ub = h;
+
+    for(int i = 0 ; i < n ; i++){
+        lb -= 1;
+        ub += 1;
+        
+        int time = v[i][0];
+        int allowed_lb = v[i][1];
+        int allowed_ub = v[i][2];
+
+        if(lb < allowed_lb || ub > allowed_ub){
+            cout<<"No"<<endl;
+            return 0;
+        }
+    }
+
+    cout<<"Yes"<<endl;
+    
+    return 0;
+}
